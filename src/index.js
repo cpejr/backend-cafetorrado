d3bb321b0443fbb4261f04af5197bbee6f3e699e
Igor Amoras <igorduarteamoras@gmail.com>
2021-02-04 22:28:18 -0300

    Feat: Mudança para socket.io
    Implementações de tempo e trabalho com arrays

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
-const webSocketServer = require('websocket').server;
-const http = require('http');
+const content = require('fs').readFileSync(`${__dirname}/index.html`, 'utf8');
 
-const webSocketServerPort = 9000;
-
-const server = http.createServer();
-server.listen(webSocketServerPort);
-console.log('listening on port 9000');
-
-const getUniqueID = () => 1;
-// const s4 = () => Math.floor((1 + Math.random()).toString(16).substring(1));
-const wsServer = new webSocketServer({
-  httpServer: server,
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(content));
+  res.end(content);
 });
 
-const clients = {};
+// const buffer = new ArrayBuffer(16);
+// const int32View = new Int32Array(buffer);
 
-wsServer.on('request', (request) => {
-  const userID = getUniqueID();
-  console.log(`${new Date()} Received new connection from origin ${request.origin} . `);
+const io = require('socket.io')(httpServer);
 
-  const connection = request.accept(null, request.origin);
-  clients[userID] = connection;
-  console.log(`connected: ${userID}in${Object.getOwnPropertyNames(clients)}`);
-
-  connection.on('message', (message) => {
-    console.log(message);
-    if (message.type === 'utf8') {
-      console.log('Received Message: ', message.utf8Data);
-      for (const key in clients) {
-        clients[key].sendUTF(message.utf8Data);
-        console.log('send message to: ', clients[key]);
-      }
-    }
+io.on('connection', (socket) => {
+  console.log('connect');
+  socket.on('hey', (data) => {
+    console.log('hey', data);
   });
+  let counter = 1;
+  setInterval(() => {
+    // if (counter <= int32View.Length) { counter = 0; }
+    // int32View[counter] = counter;
+    // console.log(int32View);
+    socket.emit('hello', ++counter);
+  }, 200);
+});
+httpServer.listen(3000, () => {
+  console.log('go to http://localhost:3000');
 });
