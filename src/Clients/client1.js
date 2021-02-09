f031fad91d0c792975fd8908d30764cf4328b868
Giovanni Martins <martinsgio95@gmail.com>
2021-02-08 21:48:07 -0300

    Feat: Implementação dos Clients Tcp Socket

---
+++ src/Clients/client1.js
@@ -0,0 +1,22 @@
+// Client 1: utilizara a porta 888, que ira configurar a maquina 
+
+const net = require('net');
+
+const client1 = new net.Socket();
+
+client1.connect(888, '192.164.4.1', () => {
+  console.log('Client 1: connection established with server');
+  client1.write('Connected');
+});
+
+client1.setEncoding('utf-8');
+
+client1.on('data', (data) => {
+  console.log(`Data from server: ${data}`);
+});
+
+client1.on('close', () => {
+  console.log('Connection closed');
+});
+
+module.exports = client1;
---
+++ src/Clients/client2.js
@@ -0,0 +1,22 @@
+// Client 2: utilizara a porta 555, recebera os parametros da maquina
+
+const net = require('net');
+
+const client2 = new net.Socket();
+
+client2.connect(555, '192.164.4.1', () => {
+  console.log('Client 2: connection established with server');
+  client2.write('Connected');
+});
+
+client2.setEncoding('utf-8');
+
+client2.on('data', (data) => {
+  console.log(`Data from server: ${data}`);
+});
+
+client2.on('close', () => {
+  console.log('Connection closed');
+});
+
+module.exports = client2;
