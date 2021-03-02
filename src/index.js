789a26dfd2413f075834b88477c49891720ce5fe
Igor Amoras <igorduarteamoras@gmail.com>
2021-03-02 18:25:34 -0300

    Feat: base do fileReader em TXT com streaming pro front

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const cors = require('cors');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 const EventEmitter = require('events');
 
+const port = process.env.PORT || 3333;
 const app = express();
 const server = require('http').createServer(app);
+
+const emitter = new EventEmitter();
+emitter.setMaxListeners(1);
+
 let io = require('socket.io')(server, {
   cors: {
     origin: '*',
   },
 });
+const routes = require('./routes');
 
-const emitter = new EventEmitter();
-emitter.setMaxListeners(1);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(port, () => {
+  console.log(`Is listening on ${port}`);
+});
 
-/*eslint-disable */
+// eslint-disable-next-line
 var bufferData = {
-  waterTemp:0,
+  waterTemp: 0,
   fireTemp: 0,
   ROR: 0,
   pressure: 0,
@@ -22,7 +36,8 @@ var bufferData = {
   grainyness: 0,
   time: 0,
 };
-let x = 2
+
+let x = 2;
 function addData(buffer) {
   buffer.waterTemp = Math.cos(x);
   buffer.fireTemp = -Math.sin(x);
@@ -33,39 +48,64 @@ function addData(buffer) {
   x += 0.1;
 }
 
-
 app.get('/', (req, res) => {
-  res.send('Connected'); 
+  res.send('Connected');
 });
 
-
-
-io.on('connection', (socket) => { 
-  let loopData; 
+io.on('connection', (socket) => {
+  let loopData;
   loopData = setInterval(() => {
     io.to(socket.id).emit('newData', bufferData);
     addData(bufferData);
   }, 200);
+
   socket.on('cleanList', () => {
-    socket.off('newData', () => {})
-  })
+    socket.off('newData', () => {});
+  });
+
+  fs.readFile((path.join(__dirname), 'buttonData.txt'), 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+      return;
+    }
+    setInterval(() => {
+      socket.emit('txtFile', data);
+    }, 1000);
+  });
+
   socket.on('manualData', (data) => {
     console.log(data);
-  })
+  });
 
   socket.on('dataFromButton', (data) => {
     bufferData.ROR = data;
-    console.log(data)  
-  })
+    console.log(data);
+    fs.appendFile((path.join(__dirname), 'buttonData.txt'), `${data} \n`, (err) => {
+      if (err) throw err;
+    });
+  });
 
   socket.on('disconnect', () => {
-    console.log('cleaning ' + socket.id);
+    console.log(`cleaning ${socket.id}`);
     clearInterval(loopData);
-  })
+  });
 });
 
-
-
-
+setInterval(() => {
+  const { freemem, totalmem } = os;
+  const total = parseInt(totalmem() / 1024 / 1024);
+  const mem = parseInt(freemem() / 1024 / 1024);
+  const percents = parseInt((mem / total) * 100);
+  const pcStats = {
+    free: `${mem} MB`,
+    total: `${total} MB`,
+    usage: `${percents}%`,
+  };
+  console.clear();
+  console.log('/********Real-time usage of PC RAM*********/');
+  console.table(pcStats);
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+  console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+}, 1000);
 io = io.listen(server);
 server.listen(8888);
