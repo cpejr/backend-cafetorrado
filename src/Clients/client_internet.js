e08bb726547598411ee91aaae418e11f55f5b237
Igor Amoras <igorduarteamoras@gmail.com>
2021-04-26 21:07:20 -0300

    feat: error treament

--- src/Clients/client_internet.js
+++ src/Clients/client_internet.js
@@ -8,20 +8,25 @@ const { wifiBufferToData } = require('../Structs/wifi_converter');
 let formattedData;
 async function connectWifi() {
   const client = new net.Socket();
-  return new Promise((resolve) => {
-    client.connect(555, '192.168.5.1', () => {
-      console.log('Client: Wifi connection established with server');
-      client.write('Connected');
 
-      client.on('close', () => {
-        console.log('Wifi configuration connection closed');
-      });
+  return new Promise((resolve, reject) => {
+    try {
+      client.connect(555, '192.168.5.1', () => {
+        console.log('Client: Wifi connection established with server');
+        client.write('Connected');
 
-      client.on('data', (data) => {
-        formattedData = wifiBufferToData(data);
-        resolve([client, formattedData]);
+        client.on('close', () => {
+          console.log('Wifi configuration connection closed');
+        });
+
+        client.on('data', (data) => {
+          formattedData = wifiBufferToData(data);
+          resolve([client, formattedData]);
+        });
       });
-    });
+    } catch (error) {
+      reject(error);
+    }
   });
 }
 
