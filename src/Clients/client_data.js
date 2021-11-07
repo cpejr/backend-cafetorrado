177c9b22ed837e2661d7b023d90b8c2919f441fb
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2021-11-06 21:30:42 -0300

    backend estável, add logs no socket

--- src/Clients/client_data.js
+++ src/Clients/client_data.js
@@ -11,59 +11,70 @@ const { safeEject } = require('./errorTreatment')
 
 let separator = '';
 
+let after = Date.now();
+
+let isFirst = true;
+
 async function connectData() {
-  fs.mkdir(path.join('src/RoastArchive', 'TEMPORARY'), 0777, (err) => { 
-    if(err) {
+  fs.mkdir(path.join('src/RoastArchive', 'TEMPORARY'), 0777, (err) => {
+    if (err) {
       fs.truncateSync(path.join('src/RoastArchive/TEMPORARY/DataStructs'), 0)
       fs.truncateSync(path.join('src/RoastArchive/TEMPORARY/ParsedData.json'), 0)
     }
-    fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'DataStructs'), '', (err) => { if(err) throw err; })
-    fs.appendFile(path.join('src/RoastArchive/TEMPORARY','ParsedData.json'), '[\n', (err) => { if(err) throw err; })
+    fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'DataStructs'), '', (err) => { if (err) throw err; })
+    fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'ParsedData.json'), '[\n', (err) => { if (err) throw err; })
   })
 
   const client = new net.Socket();
-  safeEject.run( () => { 
-  try{
-    client.connect(888, '192.168.5.1', () => {
+  safeEject.run(() => {
+    try {
+      client.connect(888, '192.168.5.1', () => {
 
-      console.log('Client 1: Data connection established with server');
+        console.log('Client 1: Data connection established with server');
 
-      client.on('close', () => {
-        fs.appendFile(path.join('src/RoastArchive/TEMPORARY','ParsedData.json'), '\n]', (err) => { if(err) throw err; })
-        separator = '';
-        client.destroy();
-        console.log('Data connection closed');
+        client.on('close', () => {
+          fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'ParsedData.json'), '\n]', (err) => { if (err) throw err; })
+          separator = '';
+          client.destroy();
+          console.log('Data connection closed');
+        });
+
+        client.on('data', (data) => {
+
+          if (isFirst) {
+            isFirst = false;
+            return; // ignora o primeiro
+          }
+
+          const unpacked = unpack_memvar_t(data);
+          console.log("Dado que chega = ", Date.now() - after, data.toString('hex'));
+
+          const headers = {
+            BlkBegVin: unpacked.get('BlkBegVin').toString(16),
+            BlkEndVin: unpacked.get('BlkEndVin').toString(16),
+            BlkBegVou: unpacked.get('BlkBegVou').toString(16),
+            BlkEndVou: unpacked.get('BlkEndVou').toString(16),
+          }
+          if (headers.BlkBegVin === 'aaaaaaaa' && headers.BlkEndVin === 'bbbbbbbb' &&
+            headers.BlkBegVou === 'cccccccc' && headers.BlkEndVou === 'dddddddd') {
+            io.emit('realData', unpacked);
+            console.log(unpacked.fields.MdlModSts)
+            console.log(unpacked.fields.MdlManChr)
+
+            fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'DataStructs'), data, (err) => { if (err) throw err; })
+            fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'ParsedData.json'), separator + JSON.stringify(unpacked.fields), 'utf-8', (err) => { if (err) throw err; })
+            if (!separator) separator = ',\n';
+            console.log("Dado que envia = ", send_vin_t());
+            client.write(send_vin_t())
+
+            after = Date.now();
+          }
+        });
       });
-
-      client.on('data', (data) => {
-        console.log(data.toString('hex'));
-        const unpacked = unpack_memvar_t(data);
-        const headers = {
-          BlkBegVin: unpacked.get('BlkBegVin').toString(16),
-          BlkEndVin: unpacked.get('BlkEndVin').toString(16),
-          BlkBegVou: unpacked.get('BlkBegVou').toString(16),
-          BlkEndVou: unpacked.get('BlkEndVou').toString(16),
-        }
-          console.log(headers);
-          if(headers.BlkBegVin === 'aaaaaaaa' && headers.BlkEndVin === 'bbbbbbbb' &&
-           headers.BlkBegVou === 'cccccccc' && headers.BlkEndVou === 'dddddddd') {
-          io.emit('realData', unpacked);
-          console.log(unpacked.fields.MdlModSts)
-          console.log(unpacked.fields.MdlManChr)
-
-          fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'DataStructs'), data, (err) => { if(err) throw err; })
-          fs.appendFile(path.join('src/RoastArchive/TEMPORARY', 'ParsedData.json'), separator + JSON.stringify(unpacked.fields), 'utf-8', (err) => { if(err) throw err; })  
-          if(!separator) separator = ',\n';
-          console.log(send_vin_t())
-          client.write(send_vin_t())
-        }
-        
-      });
-    });
-  }catch(err) {
-    console.error(err)
-  }
-})
+    } catch (err) {
+      console.error(err)
+    }
+  })
   return client;
 }
 
